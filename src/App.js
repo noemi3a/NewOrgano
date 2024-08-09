9b9f2f26d5683c0fc061d74344361907167d0ee9
noemi3a <nhenrique@3anet.com.br>
2024-08-08 23:31:23 -0300

    aula1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/CampoTexto';
+import Formulario from './componentes/Formulario/Formulario';
 
 function App() {
   return (
     <div className="App">
-      Olá mundo!
+      
+      <Banner />
+      <Formulario/>
+      
     </div>
   );
 }
-
-export default App;
+export default App
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+             <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+       
+    );
+}
+
+export default Banner;  // Agora é uma exportação padrão
